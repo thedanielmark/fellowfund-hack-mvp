6383d92ff140ed6144ba6819f6ac19ab9619a031
Michael Bucher <bucher_michael@hotmail.com>
2024-11-16 03:14:40 +0700

    Add basic steps in createFellowship

--- contracts/src/FellowFund.sol
+++ contracts/src/FellowFund.sol
@@ -5,6 +5,8 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Fellowship} from "./Types.sol";
 
 interface IFellowFund {
+    event FellowshipCreated(uint256 indexed fellowshipId, Fellowship fellowship);
+
     function createFellowship(Fellowship calldata _fellowship) external;
     function applyToFellowship(string memory someData) external;
     function openFellowshipMarket() external;
@@ -20,19 +22,23 @@ interface IVerifier {
 contract FellowFund is IFellowFund, Ownable {
     IVerifier vLayerVerifierContract;
 
+    uint256 fellowshipsCount = 0;
+    mapping(uint256 fellowshipId => Fellowship) fellowships;
+
     constructor() Ownable(msg.sender) {}
 
     function createFellowship(Fellowship calldata _fellowship) external {
+        fellowshipsCount++;
+        fellowships[fellowshipsCount] = _fellowship;
         // Todo: Create a new fellowship
         // - Define liquidity - make sure funds are there - could be fixed
         // - Define how much percentage should be used for initial liquidity for applicants - could be fixed
         // - Move liquidity to fellowship (depends if fellowship is a separate contract or it's just handled here in this contract)
 
-        // Optional:
-        // - Define number of applicants
+        emit FellowshipCreated(0, _fellowship);
     }
 
-    function applyToFellowship(string memory someData) external {
+    function applyToFellowship(string memory proof) external {
         // vlayer proof is web proof = impact metric proof
         // Internally call the verifier contract with the proof and check if valid
         // -> createMarket(metadata(stringified JSON)) - internal method
