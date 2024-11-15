72b787b4006444e22106eaf67922faf21a41b766
Michael Bucher <bucher_michael@hotmail.com>
2024-11-16 04:16:46 +0700

    Init simple test setup with foundry

--- contracts/test/FellowshipFund.t.sol
+++ contracts/test/FellowshipFund.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import "lib/forge-std/src/Test.sol";
 import {FellowFund} from "../src/FellowFund.sol";
+import {Fellowship, WeightedMetric} from "../src/Types.sol";
 
 contract FellowFundTest is Test {
     FellowFund fellowFund;
@@ -11,8 +12,18 @@ contract FellowFundTest is Test {
         fellowFund = new FellowFund();
     }
 
-    function testCreateFellowship() public view {
-        string memory expected = "Hello, there!";
-        assertEq(fellowFund.createFellowship(), expected);
+    function testCreateFellowship() public {
+        WeightedMetric memory githubCommitMetric = WeightedMetric({weight: 1, count: 10});
+        WeightedMetric memory poapMetric = WeightedMetric({weight: 2, count: 20});
+        WeightedMetric[2] memory requiredMetrics;
+        requiredMetrics[0] = githubCommitMetric;
+        requiredMetrics[1] = poapMetric;
+        Fellowship memory fellowship = Fellowship({
+            name: "TestFellowship",
+            description: "This is a test fellowship",
+            liquidity: 1e23,
+            requiredMetrics: requiredMetrics
+        });
+        fellowFund.createFellowship(fellowship);
     }
 }
