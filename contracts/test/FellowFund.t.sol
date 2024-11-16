638e8d5fc57bb8c55610e1a3cce7c1d60be36c9a
Michael Bucher <bucher_michael@hotmail.com>
2024-11-16 16:09:54 +0700

    Fix foundry test setup

--- contracts/test/FellowFund.t.sol
+++ contracts/test/FellowFund.t.sol
@@ -3,27 +3,39 @@ pragma solidity ^0.8.0;
 
 import "lib/forge-std/src/Test.sol";
 import {FellowFund} from "../contracts/FellowFund.sol";
-import {Fellowship, WeightedMetric} from "../contracts/Types.sol";
+import "../contracts/Types.sol";
 
 contract FellowFundTest is Test {
+    uint256 private constant ONE_SECOND = 1;
+    uint256 private constant ONE_MINUTE = 60 * ONE_SECOND;
+    uint256 private constant ONE_HOUR = 60 * ONE_MINUTE;
+    uint256 private constant ONE_DAY = 24 * ONE_HOUR;
+    uint256 private constant ONE_WEEK = 7 * ONE_DAY;
+    uint256 private constant ONE_MONTH = 30 * ONE_DAY;
+    uint256 private constant ONE_YEAR = 365 * ONE_DAY;
     FellowFund fellowFund;
 
+    address private constant VERIFIER = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    address private constant OPERATOR = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+
     function setUp() public {
-        fellowFund = new FellowFund();
+        fellowFund = new FellowFund(VERIFIER, OPERATOR);
     }
 
-    function testCreateFellowship() public {
-        WeightedMetric memory githubCommitMetric = WeightedMetric({weight: 1, count: 10});
-        WeightedMetric memory poapMetric = WeightedMetric({weight: 2, count: 20});
-        WeightedMetric[2] memory requiredMetrics;
-        requiredMetrics[0] = githubCommitMetric;
-        requiredMetrics[1] = poapMetric;
+    function testCreateFellowship() public payable {
+        uint256 applicationDeadline = block.timestamp + ONE_MINUTE;
+        uint256 marketDeadline = applicationDeadline + ONE_MINUTE;
+        uint256 epochEndTime = marketDeadline + ONE_MINUTE;
+        uint256 funds = 1e20;
         Fellowship memory fellowship = Fellowship({
-            name: "TestFellowship",
-            description: "This is a test fellowship",
-            liquidity: 1e23,
-            requiredMetrics: requiredMetrics
+            metadata: "TestFellowship",
+            funds: funds,
+            applicationDeadline: applicationDeadline,
+            marketDeadline: marketDeadline,
+            epochEndTime: epochEndTime,
+            status: FellowshipStatus.Created,
+            maxApplicants: 10
         });
-        fellowFund.createFellowship(fellowship);
+        fellowFund.createFellowship{value: funds}(fellowship);
     }
 }
