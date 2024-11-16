44cd83a57275c9fc1def9233fa93baa462ba68fd
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-11-16 17:04:58 +0700

    added basic push functions

---
+++ api/functions/push/broadcastNotification.js
@@ -0,0 +1,32 @@
+const { PushAPI, CONSTANTS } = require("@pushprotocol/restapi");
+const { ethers } = require("ethers");
+
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://eth-sepolia.g.alchemy.com/v2/cF6vdciZhe1Kdk63D63NfrJXeBvna1d0"
+);
+const signer = new ethers.Wallet(
+  "6bec59d4979fdaaf7f4b7174b84332246fb89e42b159e930bf7ea2351483b5a0",
+  provider
+);
+
+async function broadcastNotification(type, body) {
+  const userAlice = await PushAPI.initialize(signer, {
+    env: CONSTANTS.ENV.STAGING,
+  });
+
+  if (type == 1) {
+    const sendNotifRes = await userAlice.channel.send(["*"], {
+      notification: { title: "New Fellow Applied", body: body, category: 1 },
+    });
+  } else {
+    const sendNotifRes = await userAlice.channel.send(["*"], {
+      notification: {
+        title: "New Betting has been placed",
+        body: body,
+        category: 2,
+      },
+    });
+  }
+}
+
+broadcastNotification(2, "A new fellow has applied to your fellowship");
